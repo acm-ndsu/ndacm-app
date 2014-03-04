4f77a459e0a4a8ba8c06c126a1a7d4dfe4f87eb4
theironmarx <the.iron.marx@gmail.com>
2014-03-04 17:59:19 -0600

    Renamned menu button IDs

--- NDACM App/src/org/ndacm/ndacmapp/MenuActivity.java
+++ NDACM App/src/org/ndacm/ndacmapp/MenuActivity.java
@@ -1,15 +1,20 @@
 package org.ndacm.ndacmapp;
 
 import android.os.Bundle;
+import android.R.menu;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View.OnClickListener;
 
 public class MenuActivity extends Activity {
 
+			
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		
+		
 	}
 
 	@Override
