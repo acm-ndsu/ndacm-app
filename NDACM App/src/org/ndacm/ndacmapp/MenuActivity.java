1a6998f9d9b1109e6e996b37a7b18d33a13ff8d9
theironmarx <the.iron.marx@gmail.com>
2014-03-04 18:19:54 -0600

    Linked up buttons with onclicklisteners

--- NDACM App/src/org/ndacm/ndacmapp/MenuActivity.java
+++ NDACM App/src/org/ndacm/ndacmapp/MenuActivity.java
@@ -1,10 +1,12 @@
 package org.ndacm.ndacmapp;
 
-import android.os.Bundle;
-import android.R.menu;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuActivity extends Activity {
 
@@ -14,8 +16,71 @@ public class MenuActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
 		
+		TextView menuButtonPreferences = (TextView) findViewById(R.id.menu_textview_preferences);
+		menuButtonPreferences.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MenuActivity.this, "Preferences :)", Toast.LENGTH_SHORT).show();
+			}
+		});
 		
+		TextView menuButtonRoster = (TextView) findViewById(R.id.menu_textview_roster);
+		menuButtonRoster.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MenuActivity.this, "Roster :)", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		TextView menuButtonEvents = (TextView) findViewById(R.id.menu_textview_events);
+		menuButtonEvents.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MenuActivity.this, "Events :)", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		TextView menuButtonAbout = (TextView) findViewById(R.id.menu_textview_about);
+		menuButtonAbout.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MenuActivity.this, "About Us :)", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		TextView menuButtonCredit = (TextView) findViewById(R.id.menu_textview_credit);
+		menuButtonCredit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MenuActivity.this, "Credit System :)", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		TextView menuButtonCamera = (TextView) findViewById(R.id.menu_textview_camera);
+		menuButtonCamera.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MenuActivity.this, "ACM Camera:)", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		TextView menuButtonExtra = (TextView) findViewById(R.id.menu_textview_extra);
+		menuButtonExtra.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MenuActivity.this, "Extra button :o", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
+	
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
