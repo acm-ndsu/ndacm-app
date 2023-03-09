fd20c3916ce6c9d6a267a5981705857c7de0bc48
Carson <millercarson111@gmail.com>
2023-03-09 13:48:29 -0600

    Fixed Card View for snacks.

--- app/src/main/java/io/github/acm_ndsu/acmapp/Snacks.kt
+++ app/src/main/java/io/github/acm_ndsu/acmapp/Snacks.kt
@@ -1,8 +1,5 @@
 package io.github.acm_ndsu.acmapp
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,26 +8,13 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
-class Snacks : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            // Calling the composable function
-            // to display element and its contents
-            Mycontent()
-        }
-    }
-}
 
 // Creating a composable function to create a simple card
 // Calling this function as content in the above function
 @Composable
-fun Mycontent(){
+fun Snacks(){
     Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         Card{
             Column(Modifier.padding(10.dp)) {
@@ -39,12 +23,4 @@ fun Mycontent(){
             }
         }
     }
-}
-
-// For displaying preview in the
-// Android Studio IDE emulator
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Snacks()
 }
